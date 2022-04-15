e89594eb25fefa94ee96fe7b4e3b1b269c2a2f49
GrootOfSDUST <1219671600@qq.com>
2022-04-15 10:05:58 +0800

    修改mem_wb模块

--- vsrc/mem_wb.v
+++ vsrc/mem_wb.v
@@ -1,7 +1,7 @@
 /*
  * @Author: Groot
  * @Date: 2022-04-09 18:01:23
- * @LastEditTime: 2022-04-14 15:41:51
+ * @LastEditTime: 2022-04-15 10:04:46
  * @LastEditors: Groot
  * @Description:
  * @FilePath: /openMIPS/vsrc/mem_wb.v
@@ -16,18 +16,31 @@ module mem_wb (input wire clk,
                input wire[`RegBus] mem_wdata,
                output reg[`RegAddBus] wb_wd,
                output reg wb_wreg,
-               output reg[`RegBus] wb_wdata);
+               output reg[`RegBus] wb_wdata,
+
+               input wire mem_whilo,
+               input wire[`RegBus] mem_hi,
+               input wire[`RegBus] mem_lo,
+
+               output reg wb_whilo,
+               output reg[`RegBus] wb_hi,
+               output reg[`RegBus] wb_lo
+               );
     
     always @(posedge clk) begin
         if (rst == `RstEnable) begin
             wb_wd    <= `NOPRegAddr;
             wb_wreg  <= `WriteDisable;
             wb_wdata <= `ZeroWord;
+            wb_whilo <= `WriteDisable;
         end
         else begin
             wb_wd    <= mem_wd;
             wb_wreg  <= mem_wreg;
             wb_wdata <= mem_wdata;
+            wb_whilo <= mem_whilo;
+            wb_hi    <= mem_hi;
+            wb_lo    <= mem_lo;
         end
     end
     
